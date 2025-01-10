7b1a7bb8f0e94f5cbc031bc15b9fc994fb57cb4c
degalasujatha <degalasujatha01@gmail.com>
2025-01-10 02:27:06 -0800

    Files are added

--- src/Lab4.java
+++ src/Lab4.java
@@ -3,8 +3,8 @@ public class Lab4 {
        //Package->Class->Main Method->Statement(instructions)
         System.out.println(2+2);
         System.out.println(2*2);
-        System.out.println(2/2);
-        System.out.println(2-2);
+        System.out.println(1);
+        System.out.println(0);
         System.out.println(3/2);
     }
 }
