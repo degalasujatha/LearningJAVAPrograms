ee46337f13faf1193674d415f4d2155b10b25030
Degala Sujatha <degalasujatha01@gmail.com>
2025-01-10 02:30:09 -0800

    Add files via upload

---
+++ Lab4.java
@@ -0,0 +1,10 @@
+public class Lab4 {
+    public static void main(String[] args) {
+       //Package->Class->Main Method->Statement(instructions)
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(1);
+        System.out.println(0);
+        System.out.println(3/2);
+    }
+}
